ed7f120b0fe98db6253ba7767806f417d5c3ef98
loc <nguyenhtloc110@gmail.com>
2026-02-19 15:33:39 +0700

    udpate project

--- Dags/test_conn.py
+++ Dags/test_conn.py
@@ -5,6 +5,6 @@ from datetime import datetime
 with DAG('test_ket_noi_postgres', start_date=datetime(2026, 1, 1), schedule_interval=None, catchup=False) as dag:
     test_task = PostgresOperator(
         task_id='chay_thu_query',
-        postgres_conn_id='ten_connection_ban_vua_dat', # Điền Connection ID của bạn vào đây
+        postgres_conn_id='my_postgres_conn', # Điền Connection ID của bạn vào đây
         sql="SELECT 1;"
     )
