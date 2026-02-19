b7594d6547d43d3a8b2ffa0870c9d330207a5ffe
loc <nguyenhtloc110@gmail.com>
2026-02-19 15:23:18 +0700

    udpate project

---
+++ Dags/test_conn.py
@@ -0,0 +1,10 @@
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime
+
+with DAG('test_ket_noi_postgres', start_date=datetime(2026, 1, 1), schedule_interval=None, catchup=False) as dag:
+    test_task = PostgresOperator(
+        task_id='chay_thu_query',
+        postgres_conn_id='ten_connection_ban_vua_dat', # Điền Connection ID của bạn vào đây
+        sql="SELECT 1;"
+    )
