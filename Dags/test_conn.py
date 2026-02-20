c2eb2730f43101fdea0ac8bcb878cf865d3cb34f
loc <nguyenhtloc110@gmail.com>
2026-02-21 00:17:33 +0700

    udpate project

--- Dags/test_conn.py
+++ Dags/test_conn.py
@@ -1,10 +1,16 @@
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from datetime import datetime
 
-with DAG('test_ket_noi_postgres', start_date=datetime(2026, 1, 1), schedule_interval=None, catchup=False) as dag:
-    test_task = PostgresOperator(
-        task_id='chay_thu_query',
-        postgres_conn_id='my_postgres_conn', # Điền Connection ID của bạn vào đây
+with DAG(
+    dag_id="test_ket_noi_postgres",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,  # Airflow 3 dùng schedule thay cho schedule_interval
+    catchup=False,
+) as dag:
+
+    test_task = SQLExecuteQueryOperator(
+        task_id="chay_thu_query",
+        conn_id="my_postgres_conn",   # đổi từ postgres_conn_id
         sql="SELECT 1;"
     )
